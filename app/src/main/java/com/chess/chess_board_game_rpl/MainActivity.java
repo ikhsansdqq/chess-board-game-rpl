24c8662a74dcff5e8fdedce4cfd8987509ccaeac
Ikhsan Assidiqie <91542488+ikhsansdqq@users.noreply.github.com>
2023-10-25 21:17:13 +0700

    initial commit

--- app/src/main/java/com/chess/chess_board_game_rpl/MainActivity.java
+++ app/src/main/java/com/chess/chess_board_game_rpl/MainActivity.java
@@ -1,8 +1,9 @@
 package com.chess.chess_board_game_rpl;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
