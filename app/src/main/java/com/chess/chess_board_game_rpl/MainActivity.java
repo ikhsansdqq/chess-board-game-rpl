a628f449bf09b870f24da0d475e93c982fc4347f
Ikhsan Assidiqie <91542488+ikhsansdqq@users.noreply.github.com>
2023-10-26 21:16:29 +0700

    adding custom dialog in the MainActivity

--- app/src/main/java/com/chess/chess_board_game_rpl/MainActivity.java
+++ app/src/main/java/com/chess/chess_board_game_rpl/MainActivity.java
@@ -1,9 +1,12 @@
 package com.chess.chess_board_game_rpl;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,11 +16,31 @@ import androidx.appcompat.widget.Toolbar;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    Button mDialogButton, dismissButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDialogButton = findViewById(R.id.search_opponent);
+        Dialog dialog = new Dialog(MainActivity.this);
+
+        mDialogButton.setOnClickListener(v -> {
+            dialog.setContentView(R.layout.custom_dialog_1);
+
+            Objects.requireNonNull(dialog.getWindow()).setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.setCancelable(false);
+            Objects.requireNonNull(dialog.getWindow()).getAttributes().windowAnimations = R.style.animation;
+
+            dismissButton = dialog.findViewById(R.id.dismiss_button);
+            dismissButton.setOnClickListener(v1 -> {
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this, "okay clicked", Toast.LENGTH_SHORT).show();
+            });
+
+            dialog.show();
+        });
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         myToolbar.setTitle("Chess Board");
