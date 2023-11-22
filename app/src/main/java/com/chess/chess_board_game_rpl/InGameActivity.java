56aa1693bd5b62438027d1de79c5f8cdbc2cd4bc
CakZemprongzz <fauzanrizqimuhammad@gmail.com>
2023-11-23 05:42:26 +0700

    Reimplement ChessBoard - Chess Board has been reworked (not sure if its good implementation) - Adding button on square to response with click
    Keep in mind the pawn still not moveable yet.
    
    To see the chess board use normal mode..

--- app/src/main/java/com/chess/chess_board_game_rpl/InGameActivity.java
+++ app/src/main/java/com/chess/chess_board_game_rpl/InGameActivity.java
@@ -1,14 +1,11 @@
 package com.chess.chess_board_game_rpl;
 
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.widget.GridLayout;
-import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ImageView;
 
-import androidx.appcompat.widget.Toolbar;
-
-import java.util.Objects;
+import androidx.appcompat.app.AppCompatActivity;
 
 /* public class InGameActivity extends AppCompatActivity {
 
@@ -48,33 +45,58 @@ public class InGameActivity extends AppCompatActivity {
     }
 
     private void setupGameBoard() {
+        //GameBoard gameBoard = new GameBoard();
         GridLayout chessBoard = findViewById(R.id.chessBoard);
-        int totalSquares = 8 * 8; // total squares for an 8x8 chessboard
 
-        for (int i = 0; i < totalSquares; i++) {
-            ImageView square = new ImageView(this);
-            // Set the background color or image depending on the square's color
-            if ((i / 8) % 2 == (i % 8) % 2) {
-                square.setBackgroundColor(getResources().getColor(R.color.light_square_color)); // Define this color in your colors.xml
-            } else {
-                square.setBackgroundColor(getResources().getColor(R.color.dark_square_color)); // Define this color in your colors.xml
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                ImageView square = new ImageView(this);
+
+                /* Possible another implementation
+
+
+                Square gameSquare = gameBoard.getSquare(row, col);
+
+                // Set the image based on the state of the square in the GameBoard
+                if (gameSquare.hasPiece()) {
+                ChessPiece piece = gameSquare.getPiece();
+                if (piece instanceof Pawn && piece.getColor() == Color.BLACK) {
+                square.setImageResource(R.drawable.pawn_black);
+                }
+                // Add similar conditions for other types of pieces
+                }
+                 */
+                // Set the background color or image depending on the square's color
+                if ((row + col) % 2 == 0) {
+                    square.setBackgroundColor(getResources().getColor(R.color.light_square_color)); // Light color for even sum
+                } else {
+                    square.setBackgroundColor(getResources().getColor(R.color.dark_square_color)); // Dark color for odd sum
+                }
+
+                if ((row == 1)){
+                    square.setImageResource(R.drawable.pawn_black);
+                }
+
+                /*
+                final int finalRow = row;
+                final int finalCol = col;
+                */
+                square.setOnClickListener(v -> square.setBackgroundColor(getResources().getColor(R.color.black)));
+
+                // Set the image for the piece if there is one
+
+
+                // Add LayoutParams to control the size of the squares
+                GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+                params.width = getResources().getDimensionPixelSize(R.dimen.chess_square_size); // Define this dimension in your dimens.xml
+                params.height = getResources().getDimensionPixelSize(R.dimen.chess_square_size); // Define this dimension in your dimens.xml
+                square.setLayoutParams(params);
+
+                // Finally, add the view to your GridLayout
+                chessBoard.addView(square);
             }
-
-            // Set the image for the piece if there is one
-            if (i == (1 * 8) + 0) {
-                square.setImageResource(R.drawable.pawn_black); // Replace with your actual drawable name
-            }
-            // You can use tags or a separate method to determine what piece, if any, is on this square
-
-            // Add LayoutParams to control the size of the squares
-            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-            params.width = getResources().getDimensionPixelSize(R.dimen.chess_square_size); // Define this dimension in your dimens.xml
-            params.height = getResources().getDimensionPixelSize(R.dimen.chess_square_size); // Define this dimension in your dimens.xml
-            square.setLayoutParams(params);
-
-            // Finally, add the view to your GridLayout
-            chessBoard.addView(square);
         }
     }
+    //private void onSquareClicked(int row, int col){}
 
 }
