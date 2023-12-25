6a6b002c6f90958778fe4416977dd0685a3eb17f
DrdmRandom <117142698+DrdmRandom@users.noreply.github.com>
2023-12-25 15:56:53 +0700

    Making new UI and Finishing UI - SplashScreen - HomeScreen - Connecting all UI

---
+++ app/src/main/java/com/chess/chess_board_game_rpl/HomeScreen.java
@@ -0,0 +1,37 @@
+package com.chess.chess_board_game_rpl;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.content.Intent;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard); // Make sure this is the correct layout with the chess_mode_normal ID
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Initialize the views
+        LinearLayout normalMode = findViewById(R.id.chess_mode_normal); // Replace with the correct ID of your "Normal Mode" view
+
+        // Set the click listeners for normal mode
+        normalMode.setOnClickListener(v -> {
+            // Start InGameActivity when normal mode is clicked
+            Intent intent = new Intent(HomeScreen.this, MainActivity.class);
+            // You can put extras if you need to pass some data
+            // intent.putExtra("EXTRA_KEY", "Some Data");
+            startActivity(intent);
+        });
+
+        // Set the click listeners for competitive mode
+        // ...
+
+        // Set the click listeners for fusion mode
+        // ...
+    }
+
+}
